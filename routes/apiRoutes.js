88fc02bb7df19a49d6b51d4fe6e695703cc2f638
harsha <harsha@divami.com>
2023-05-15 17:40:55 +0530

    affqfqf

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -11,6 +11,12 @@ router.get("/", (req, res) => {
   });
 });
 
+router.get("/", (req, res) => {
+  res.status(201).json({
+    message: "Welcome to the API.",
+  });
+});
+
 router.post("/login", (req, res) => {
   console.log("logging");
   console.log("you are login is successfull");
@@ -30,6 +36,27 @@ router.post("/login", (req, res) => {
   }
   res.status(201).send({message:'successfully login'})
 });
+
+router.post("/login", (req, res) => {
+  console.log("logging");
+  console.log("you are login is successfull");
+  console.log("codacy sample code ");
+  console.log("you are login is successfull");
+  console.log("codacy sample code ");
+  let a;
+  a = "jkshrjkhjhw";
+  // console.log(a);
+  for(i=0;i<10;i++)
+  {
+        for(j=0;j<=10;j++)
+        { 
+          if(i=>j)
+            console.log("i+j",i+j)
+        }
+  }
+  res.status(201).send({message:'successfully login'})
+});
+
 
 router.post("/register", async (req, res) => {});
 
