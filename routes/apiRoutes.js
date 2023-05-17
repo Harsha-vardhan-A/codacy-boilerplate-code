80f797e35360bc64c5439e972811ec2cb25db030
harsha <harsha@divami.com>
2023-05-17 11:49:38 +0530

    complexity

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -28,11 +28,31 @@ router.post("/login", (req, res) => {
   // console.log(a);
   for(i=0;i<10;i++)
   {
-        for(j=0;j<=10;j++)
+    for(j=0;j<=10;j++)
+    { 
+      for(k=0;k<=10;k++)
+      { 
+        for(l=0;l<=10;l++)
         { 
-          if(i=>j)
-            console.log("i+j",i+j)
+          for(m=0;m<=10;m++)
+          { 
+            for(n=0;n<=10;n++)
+            { 
+              if(i<j)
+              {
+                if(j<k)
+                {
+                  if(k<m)
+                  {
+                    console.log("i+j",i+j)
+                  }
+                }
+              }
+            }
+          }
         }
+      }
+    }
   }
   res.status(201).send({message:'successfully login'})
 });
